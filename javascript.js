4f7a00ba4d013e31b7145f25b9b702428dc2291b
Wilmar <wkfrechauth@gmail.com>
2024-02-21 12:07:25 +0200

    Adicionado arquivo js

---
+++ javascript.js
@@ -0,0 +1,18 @@
+// Comentário: Este é um arquivo JavaScript básico
+
+// Declaração de variáveis
+let nome = "João";
+const idade = 25;
+
+// Função para exibir uma mensagem
+function exibirMensagem() {
+    console.log("Olá, mundo!");
+}
+
+// Chamada da função
+exibirMensagem();
+
+// Event listener para um botão
+document.getElementById("meuBotao").addEventListener("click", function() {
+    console.log("O botão foi clicado!");
+});
